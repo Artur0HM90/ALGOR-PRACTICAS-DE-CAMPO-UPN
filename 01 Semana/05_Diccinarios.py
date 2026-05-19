ac81088502ea90a76487658aa94fa65723396fa0
Arturo HURTADO MÉNDEZ <arturohurtadomendez90@gmail.com>
2026-05-19 18:34:49 -0500

    diccionarios

---
+++ 01 Semana/05_Diccinarios.py
@@ -0,0 +1,19 @@
+print("=======================")
+print("Diccionarios en Python") 
+print("=======================")
+
+trabajador = {
+    "nombre": "Arturo Miguel",
+    "edad": 35,
+    "area": "estadistica",
+    "sueldo": 2500,
+}
+
+print("Información del trabajador:", trabajador)
+
+
+# trabajador["sueldo"] = 3000 # modificar el sueldo del trabajador
+
+# trabajador["correo"] = "arturo.miguel@empresa.com" # agregar correo
+
+# print("Información del trabajador:", trabajador)
