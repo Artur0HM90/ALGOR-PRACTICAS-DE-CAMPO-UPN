6736cfea981f6be02882acec6ea0e9bff32c69c5
Arturo HURTADO MÉNDEZ <arturohurtadomendez90@gmail.com>
2026-05-19 18:17:21 -0500

    4 tuplas

---
+++ 01 Semana/04_Tuplas.py
@@ -0,0 +1,10 @@
+print("==================")
+print("Tuplas en Python") 
+print("==================")
+
+lenguajes = ("Python", "Java", "C++", "JavaScript", "Rust")
+
+print("Lista de lenguajes de programación:", lenguajes)
+print("Primer lenguaje:", lenguajes[0])
+print("Último lenguaje:", lenguajes[-1])
+print("Número de lenguajes:", len(lenguajes))
