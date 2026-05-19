aad6dbb9db57a0b637a86bed2189b10a19d1eb08
Arturo HURTADO MÉNDEZ <arturohurtadomendez90@gmail.com>
2026-05-18 21:59:38 -0500

    Se cambio el nombre del archvio

---
+++ 01 Semana/01_OperacionesBasicas.py
@@ -0,0 +1,15 @@
+print("==============================")
+print("Operaciones Básicas en Python") 
+print("==============================")
+
+primerNumero = int(input("Ingrese el primer número entero: "))
+segundoNumero = int(input("Ingrese el segundo número entero: "))
+
+
+
+
+print("La suma es:", primerNumero + segundoNumero)
+print("La resta es:", primerNumero - segundoNumero)
+print("La multiplicación es:", primerNumero * segundoNumero)
+print("La división es:", primerNumero / segundoNumero)
+
