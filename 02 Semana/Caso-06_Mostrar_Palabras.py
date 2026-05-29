e259894ce1f06cf1c9ed4fb4a5122a0d5c830be5
Arturo HURTADO MÉNDEZ <arturohurtadomendez90@gmail.com>
2026-05-28 21:38:56 -0500

    Mostrar solo palabras con más de 4 letras

---
+++ 02 Semana/Caso-06_Mostrar_Palabras.py
@@ -0,0 +1,19 @@
+# Mostrar solo palabras con más de 4 letras
+
+opcion = input("¿Quieres ingresar las palabras tú mismo? (s/n): ")
+
+palabras = []
+
+print("Palabras con más de 4 letras seran elegidas:")
+
+if opcion == "s":
+    print("Ingresa 6 palabras:")
+    for i in range(6):
+        palabra = input("Ingresa una palabra: ")
+        palabras.append(palabra)
+else:
+    palabras = ["sol", "estrella", "mar", "montaña", "volcán", "río"]
+
+for palabra in palabras:
+    if len(palabra) > 4:
+        print(palabra)
